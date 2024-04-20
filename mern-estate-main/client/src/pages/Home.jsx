743671f4941e0986850b7a4764079679abcf390d
Pajorad Pajorad <pavle.maodus2004@gmail.com>
2024-04-20 02:29:49 +0200

    izmene pred pokazivanje klijentu

--- mern-estate-main/client/src/pages/Home.jsx
+++ mern-estate-main/client/src/pages/Home.jsx
@@ -288,7 +288,7 @@ export default function Home() {
                 borderRadius: "0.75rem",
                 boxShadow:
                   "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
-                border: "4px solid #ef4444",
+                border: "4px solid #9f1d21",
               }}
               src="https://images.pexels.com/photos/87223/pexels-photo-87223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
               alt="Product screenshot"
