a113434a3d5dd92eacfa1e7315e3fc748b0a4694
Banega00 <bane.stojanovic00@gmail.com>
2024-05-19 12:25:03 +0200

    minor env changes

--- mern-estate-main/client/vite.config.js
+++ mern-estate-main/client/vite.config.js
@@ -1,16 +1,28 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  console.log("ENV VARS:" , env.API_URL)
+  return {
+
+    server: {
+
+      define: {
+        __APP_ENV__: env.APP_ENV,
+      },
+
+      proxy: {
+        '/api': {
+          target: env.API_URL,
+          secure: false,
+        },
       },
     },
-  },
-
-  plugins: [react()],
-});
+  
+    plugins: [react()],
+  }
+})
