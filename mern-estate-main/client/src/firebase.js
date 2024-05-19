0c025775bc4b6d841903e52d65f5731ad19cf242
Pajorad Pajorad <pavle.maodus2004@gmail.com>
2024-05-19 15:59:49 +0200

    env + firebase transfer

--- mern-estate-main/client/src/firebase.js
+++ mern-estate-main/client/src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+import dotenv from 'dotenv';
+dotenv.config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDZD5Noy9m6rH-5g9-CpUOjMmqdUTvLJBI",
-  authDomain: "superstan-9a61f.firebaseapp.com",
-  projectId: "superstan-9a61f",
-  storageBucket: "superstan-9a61f.appspot.com",
-  messagingSenderId: "789805890190",
-  appId: "1:789805890190:web:2298bf08e2ee85a54de7df"
+  apiKey: import.meta.env.FB_API_KEY,
+  authDomain: import.meta.env.FB_AUTH_DOMAIN,
+  projectId: import.meta.env.FB_PROJECT_ID,
+  storageBucket: import.meta.env.FB_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FB_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FB_APP_ID
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
